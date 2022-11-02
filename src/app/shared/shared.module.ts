81da9371997d940c958f1d5f80496fe16cd71336
jhonrami12 <jhonrami12@gmail.com>
2022-11-01 20:21:49 -0500

    Creation component search-field

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchFieldComponent } from './components/search-field/search-field.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchFieldComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SearchFieldComponent
   ]
 })
 export class SharedModule { }
